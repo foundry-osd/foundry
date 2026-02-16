ed0d87afd2c85959f934c98a87d7c7f25a6245ab
Mickaël CHAVE <mchave3@live.fr>
2026-02-16 21:24:21 +0100

    Implement ADK banner as a user control: refactor banner logic into AdkBanner.xaml and AdkBanner.xaml.cs

---
+++ src/Foundry/Views/Controls/AdkBanner.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Foundry.Views.Controls;
+
+public partial class AdkBanner : UserControl
+{
+    public AdkBanner()
+    {
+        InitializeComponent();
+    }
+}
