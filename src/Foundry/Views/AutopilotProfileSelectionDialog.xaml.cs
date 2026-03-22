a347129bc174128cfdb8113d714f45aace014fd9
Mickaël CHAVE <mchave3@live.fr>
2026-03-22 21:33:31 +0100

    Polish Autopilot picker bindings and focus

--- src/Foundry/Views/AutopilotProfileSelectionDialog.xaml.cs
+++ src/Foundry/Views/AutopilotProfileSelectionDialog.xaml.cs
@@ -7,5 +7,16 @@ public partial class AutopilotProfileSelectionDialog : Window
     public AutopilotProfileSelectionDialog()
     {
         InitializeComponent();
+        Loaded += OnLoaded;
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        if (ProfilesListView.Items.Count > 0 && ProfilesListView.SelectedItem is null)
+        {
+            ProfilesListView.SelectedIndex = 0;
+        }
+
+        _ = ProfilesListView.Focus();
     }
 }
