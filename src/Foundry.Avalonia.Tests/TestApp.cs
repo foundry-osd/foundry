85e65d66262eefb95a53dea104e19004117b806c
Mickaël CHAVE <mchave3@live.fr>
2026-08-09 22:41:11 +0200

    feat(avalonia): add shared presentation projects

---
+++ src/Foundry.Avalonia.Tests/TestApp.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Foundry Project contributors.
+// Licensed under the MIT License.
+// See the LICENSE file in the project root for more information.
+
+using Avalonia;
+
+namespace Foundry.Avalonia.Tests;
+
+internal sealed class TestApp : Application;
